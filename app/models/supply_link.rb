e00b3966ef2fc7023c17cb58a31d3ca60ff45fca
Ryan Lue <ryan.lue@gmail.com>
2017-07-27 16:56:47 +0800

    Add join-table model associations to SupplyLink model

--- app/models/supply_link.rb
+++ app/models/supply_link.rb
@@ -2,8 +2,8 @@ class SupplyLink < ApplicationRecord
   include Confirmable
 
   validates :purchaser_id, uniqueness: { scope: :supplier_id }
-  belongs_to :purchaser, class_name: 'Company'
-  belongs_to :supplier,  class_name: 'Company'
+  belongs_to :purchaser, class_name: :Company
+  belongs_to :supplier,  class_name: :Company
 
   def self.between(a, b)
     find_by(supplier: a, purchaser: b) || find_by(supplier: b, purchaser: a)
